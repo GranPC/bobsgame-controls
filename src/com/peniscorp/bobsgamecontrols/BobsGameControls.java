65aa5e3ba54184d04854ab6c0c521e3ded57705e
Gran PC <gran.pc@gmail.com>
2014-02-17 22:18:10 +0100

    Fix code style

--- src/com/peniscorp/bobsgamecontrols/BobsGameControls.java
+++ src/com/peniscorp/bobsgamecontrols/BobsGameControls.java
@@ -9,21 +9,26 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class BobsGameControls implements IXposedHookLoadPackage {
+public class BobsGameControls implements IXposedHookLoadPackage
+{
+	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable
+	{
+        	try
+        	{
+        		if ( !lpparam.packageName.equals( "com.bobsgame.bg" ) ) return;
 
-	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        try {
-        	if ( !lpparam.packageName.equals( "com.bobsgame.bg" ) ) return;
-
-        	findAndHookMethod( "com.badlogic.gdx.controllers.android.AndroidController", lpparam.classLoader, "getName", new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod( MethodHookParam param ) throws Throwable
-                {
-                	param.setResult( "OUYA Game Controller" );
-                }
-        	});
-	    } catch (Throwable e) {
-	    	XposedBridge.log(e);
-	    }		
+        		findAndHookMethod( "com.badlogic.gdx.controllers.android.AndroidController", lpparam.classLoader, "getName", new XC_MethodHook() 
+        		{
+                		@Override
+                		protected void afterHookedMethod( MethodHookParam param ) throws Throwable
+                		{
+                			param.setResult( "OUYA Game Controller" );
+                		}
+        		} );
+	    	}
+	    	catch ( Throwable e )
+	    	{
+		    	XposedBridge.log( e );
+	    	}		
 	}
 }
